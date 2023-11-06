762b46a236041ea8992a62cc7b68cb421cb36f17
Fachreza Maulana <zzafachreza@gmail.com>
2023-11-06 16:11:49 +0700

    Finish

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -189,7 +189,7 @@ export default function Home({ navigation }) {
               textAlign: 'center',
               fontFamily: fonts.secondary[600],
               fontSize: windowWidth / 15
-            }}>{data.INDEX}</Text>
+            }}>{data.INDEX == "NaN" ? "Out of Range" : data.INDEX}</Text>
           </View>
         </View>
         <MyGap jarak={50} />
@@ -206,7 +206,16 @@ export default function Home({ navigation }) {
 
 
 
-
+      <View>
+        <Text style={{
+          textAlign: 'center',
+          padding: 10,
+          backgroundColor: colors.white,
+          color: colors.black,
+          fontSize: windowWidth / 28,
+          fontFamily: fonts.primary[600],
+        }}>Copyright © 2022 | Laboratory of Pupuk Kaltim</Text>
+      </View>
 
 
 
